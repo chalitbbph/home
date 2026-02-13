ff522e296dd966c2153e6cf7591ad681329be79e
Chalit Siangsanan <166695113+chalitbbph@users.noreply.github.com>
2026-02-13 20:44:50 +0700

    Update constants.tsx

--- constants.tsx
+++ constants.tsx
@@ -1,6 +1,18 @@
-
 export const ZONES = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
-export const COLORS = ['Red', 'Blue', 'Green', 'Yellow', 'Orange', 'Purple'];
+export const COLORS = [
+  'แดง',
+  'น้ำเงิน', 
+  'เขียว',
+  'เหลือง',
+  'ส้ม',
+  'ม่วง',
+  'ชมพู',
+  'ขาว',
+  'ดำ',
+  'เทา',
+  'น้ำตาล',
+  'ฟ้า'
+];
 export const BOX_SIZES = ['10x10x10', '20x20x20', '30x30x30', '40x40x40', '50x50x50'];
 
 export const AUTH_CONFIG = {
